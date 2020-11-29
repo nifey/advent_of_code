db8aab500b7d228f1bfc7df7da0f3d4e9f3ee58a
Abdun Nihaal <nihaal.an@gmail.com>
2020-11-29 22:26:19 +0530

    Adds Basic template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+
+mod solutions;
+
+fn aoc(day: u64) {
+    match day {
+        1 => solutions::day1::solve(),
+        _ => {}
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut params = env::args();
+    if params.len() < 2 {
+        println!("Specify the day number");
+        return;
+    }
+    if let Ok(day) = params.nth(1).unwrap().parse::<u64>() {
+        println!("=============================");
+        println!("   Advent of Code : Day {:2} ", day);
+        println!("=============================");
+        aoc(day);
+    }
 }
