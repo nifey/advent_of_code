63897257f103b889966f4ad851aad1b5c9ac7f32
Abdun Nihaal <nihaal.an@gmail.com>
2020-12-01 11:57:24 +0530

    Finished day1

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,36 @@ use std::env;
 
 mod solutions;
 
-fn aoc(day: u64) {
+fn aoc(day: u64, part: u64, filename: String) {
     match day {
-        1 => solutions::day1::solve(),
+        1 => {
+            if part == 1 {
+                solutions::day1::solve_part1(filename);
+            } else if part == 2 {
+                solutions::day1::solve_part2(filename);
+            }
+        }
         _ => {}
     }
 }
 
 fn main() {
-    let mut params = env::args();
-    if params.len() < 2 {
-        println!("Specify the day number");
+    let params: Vec<String> = env::args().collect();
+    if params.len() < 4 {
+        println!("Specify the day number, part and input filename");
         return;
     }
-    if let Ok(day) = params.nth(1).unwrap().parse::<u64>() {
-        println!("=============================");
-        println!("   Advent of Code : Day {:2} ", day);
-        println!("=============================");
-        aoc(day);
+    if let Ok(day) = params[1].parse::<u64>() {
+        if let Ok(part) = params[2].parse::<u64>() {
+            println!("=============================");
+            println!("   Advent of Code : Day {:2} ", day);
+            println!("=============================");
+            let filename = params[3].clone();
+            aoc(day, part, filename);
+        } else {
+            println!("Incorrect part number");
+        }
+    } else {
+        println!("Incorrect day number");
     }
 }
--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -1,1 +1,62 @@
-pub fn solve() {}
+use std::fs;
+
+pub fn solve_part1(filename: String) {
+    // Finding two numbers that add up to 2020
+    let input_data = fs::read_to_string(filename).expect("Could not open file for reading");
+    let mut numbers: Vec<u64> = input_data
+        .lines()
+        .map(|x| x.parse::<u64>().unwrap())
+        .collect();
+    numbers.sort();
+    let mut left = 0;
+    let mut right = numbers.len() - 1;
+    while left < right {
+        if numbers[left] + numbers[right] == 2020 {
+            println!("{}", numbers[left] * numbers[right]);
+            break;
+        } else if numbers[left] < 2020 - numbers[right] {
+            while numbers[left] < 2020 - numbers[right] {
+                left += 1;
+            }
+        } else {
+            while numbers[left] > 2020 - numbers[right] {
+                right -= 1;
+            }
+        }
+    }
+}
+
+pub fn solve_part2(filename: String) {
+    // Finding three numbers that add up to 2020
+    let input_data = fs::read_to_string(filename).expect("Could not open file for reading");
+    let mut numbers: Vec<i64> = input_data
+        .lines()
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect();
+    numbers.sort();
+    for fixed in 0..numbers.len() {
+        let target = 2020 - numbers[fixed];
+        let mut left = 0;
+        let mut right = numbers.len() - 1;
+        while left < right {
+            if numbers[left] + numbers[right] == target {
+                println!("{}", numbers[left] * numbers[right] * numbers[fixed]);
+                std::process::exit(0);
+            } else if numbers[left] < target - numbers[right] {
+                while numbers[left] < target - numbers[right] {
+                    left += 1;
+                }
+            } else {
+                while numbers[left] > target - numbers[right] {
+                    right -= 1;
+                }
+            }
+            if left == fixed {
+                left += 1;
+            }
+            if right == fixed {
+                right -= 1;
+            }
+        }
+    }
+}
